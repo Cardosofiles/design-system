1e28e98fc7b7544e733c3edd00555197d897546a
cardosofiles <cardosofiles@outlook.com>
2024-10-26 18:35:31 -0300

    initial settings

---
+++ src/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  white: "#FFF",
+  black: "#000",
+
+  gray100: "#E1E1E6",
+  gray200: "#A9A9B2",
+  gray400: "#7C7C8A",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292E",
+  gray800: "#202024",
+  gray900: "#121214",
+
+  system100: "#f4f7fc",
+  system300: "#dfe7f2",
+  system400: "#8fa4c3",
+  system700: "#586c91",
+  system900: "#283347",
+};
